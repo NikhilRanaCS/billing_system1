b85c364e1269bed22802e886259c1b98feeb5bfc
Nikhil Rana <153705473+NikhilRanaCS@users.noreply.github.com>
2024-04-07 17:38:49 +0530

    Delete BillingSystem directory

--- BillingSystem/home/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.contrib import admin
-from .models import Bill, Customer, Product, emp_detail   # Import the model from your home app
-
-admin.site.register(Customer)
-admin.site.register(Product)
-admin.site.register(Bill)# Register the model in the admin site
